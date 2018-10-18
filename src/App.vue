1ecfc8b92e1eae6ad9d54dcbf7f5c98ae0fc370c
Filip Kajetaniak <kajetaniakkfilip@gmail.com>
2018-10-18 21:03:53 +0200

    initial commit and project setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+<style lang="scss">
+@import './scss/var';
+// *{
+// ::-moz-selection { background: $yellow; }
+// ::selection { background: $yellow; }
+// box-sizing: border-box;
+// }
+body{
+  font-family: $n;
+}
+</style>
